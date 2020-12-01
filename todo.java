4c14c1874144490d72d45041b6fcd571518ba793
ayushk7102 <ayush_7102@yahoo.in>
2020-12-01 23:40:21 +0530

    Added  functionality to complete tasks

--- todo.java
+++ todo.java
@@ -10,6 +10,7 @@ public class todo
 {
   static int num_tasks =0;
   static ArrayList<String> tasks = new ArrayList<String>();
+  static boolean write_stat =true;
 
   public static void main(String[] args)
   { Scanner sc =new Scanner(System.in);
@@ -27,7 +28,7 @@ public class todo
     }
 
   public static void input(String t)
-  {
+  { write_stat = true;
     String arr[] = t.split(" ");
     switch(arr[0])
     {
@@ -70,12 +71,33 @@ public class todo
 
     catch(Exception e){break;}
 
+
+    case "$strike":
+    try
+    { if(arr.length>1)
+      {
+        int str_num = Integer.valueOf(arr[1]);
+        strike_task(str_num);
+
+      }
+
+    break;
+    }
+    catch(Exception e){break;}
+
+
+
       case "$exit":
       case "exit":
       break;
 
       default:
       System.out.println("Unrecognised command");
+
+
+
+
+
 
     }
   }
@@ -145,18 +167,117 @@ System.out.println("You have "+num_tasks+" task(s) saved todo");
         // attach a file to FileWriter
          fw=new FileWriter("todo.txt", true);
 
-        if(num_tasks!=0){fw.write('\n');}
 
         // read character wise from string and write
         // into FileWriter
         for (int i = 0; i < task.length(); i++)
             fw.write(task.charAt(i));
 
-        System.out.println("\nTask written successfully!\n");
+      if(num_tasks!=0){fw.write('\n');}
+
+        if(write_stat){
+          System.out.println("\nTask written successfully!\n");
+        }
         //close the file
         fw.close();
 
   }
+
+  public static void strike_task(int ab) throws IOException
+
+  { write_stat = false;
+
+    int sum = 0;
+
+     FileReader fr=null;
+     try
+     {
+         fr = new FileReader("todo.txt");
+     }
+     catch (FileNotFoundException fe)
+     {
+         System.out.println("File not found");
+     }
+
+     String x="";
+     String content="";
+     BufferedReader br = new BufferedReader(fr);
+     int i=1;
+
+     // read from FileReader till the end of file
+     while ((x=br.readLine())!=null )
+         {if(!x.equals(""))
+           {content+=(i+". "+x);
+           content+="\n";
+           i++;}
+
+         }
+
+     // close the file
+     fr.close();
+
+     int count = 0;
+     ArrayList<String> tas = new ArrayList<String>();
+     String d = "";
+
+     for (int a = 0 ; a<content.length(); a++)
+     {  String s = content.substring(a,a+1);
+        d+=s;
+
+       if(s.equals("\n"))
+       {  d=d.substring(0,d.length()-1);
+          d = d.substring(d.indexOf(".")+2);
+          d = "unstrike "+d;
+
+         tas.add(d);
+         d="";
+
+         sum++;
+       }
+
+
+
+     }
+
+
+
+          //System.out.println(tas);
+
+          if(ab<=tas.size() && ab>0)
+          {tas.remove(ab-1);
+
+            try
+            {   num_tasks=0;
+
+              //System.out.println(num_tasks);
+                String aa[] = {""};
+                write_task(null);
+
+                //write_task(aa);
+
+              //  System.out.println(tas);
+
+                for(int si=0;si<tas.size();si++)
+                {
+                  String ak = tas.get(si);
+                  num_tasks++;
+                  write_task(ak.split(" "));
+
+                }
+
+
+            }
+
+          catch(Exception e){System.out.println("Error while striking task");}
+
+          }
+          else{
+            System.out.println("Invalid task number");
+          }
+
+
+  }
+
 
   public static int find_num_tasks() throws IOException
   {
@@ -208,3 +329,12 @@ System.out.println("You have "+num_tasks+" task(s) saved todo");
 
 
 }
+
+/*
+Download and complete MATH PDE problem sets
+Revise PPT ELE1051 delta-star transformations
+SMS Piano sir about fee payment
+CSE 1051 HW: Algorithm flowchart of surface area & volume of sphere, cone, cylinder
+MRM Rover Assignment submit before 1159 pm
+Add strike task method in todo.java
+Do ELE 1051 hw quiz */
